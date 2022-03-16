322e47c2e2c26d7853047ca9362cff72e3fd52d9
isurojit <iamsurojit@hotmail.com>
2022-03-16 13:10:40 +0530

    used ref in useEffect

--- src/App.js
+++ src/App.js
@@ -4,31 +4,37 @@ import "./App.css";
 const App = () => {
   const [name, setName] = useState("");
   //const renderCount = useRef(0);
-  const inputRef = useRef();
+  const prevName = useRef("");
 
   const inputChangeHandler = (e) => {
     setName(e.target.value);
   };
 
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+
   // useEffect(() => {
   //   renderCount.current = renderCount.current + 1;
   // });
 
-  const focusHandler = () => {
-    inputRef.current.focus();
-  };
+  // const focusHandler = () => {
+  //   inputRef.current.focus();
+  // };
 
   return (
     <div>
       <input
-        ref={inputRef}
+        // ref={inputRef}
         type="text"
         value={name}
         onChange={inputChangeHandler}
       />
-      <h1>My Name is {name}</h1>
+      <h1>
+        My Name is {name} and it used to be {prevName.current}
+      </h1>
       {/* <h3>I have rendered {renderCount.current} times</h3> */}
-      <button onClick={focusHandler}>Focus</button>
+      {/* <button onClick={focusHandler}>Focus</button> */}
     </div>
   );
 };
