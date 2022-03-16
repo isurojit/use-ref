d41ff9112dee52adb5b003a8d43efb90c8652f38
isurojit <iamsurojit@hotmail.com>
2022-03-16 13:05:49 +0530

    input focus

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 const App = () => {
   const [name, setName] = useState("");
+  //const renderCount = useRef(0);
+  const inputRef = useRef();
 
   const inputChangeHandler = (e) => {
     setName(e.target.value);
   };
+
+  // useEffect(() => {
+  //   renderCount.current = renderCount.current + 1;
+  // });
+
+  const focusHandler = () => {
+    inputRef.current.focus();
+  };
+
   return (
     <div>
-      <input type="text" value={name} onChange={inputChangeHandler} />
+      <input
+        ref={inputRef}
+        type="text"
+        value={name}
+        onChange={inputChangeHandler}
+      />
       <h1>My Name is {name}</h1>
+      {/* <h3>I have rendered {renderCount.current} times</h3> */}
+      <button onClick={focusHandler}>Focus</button>
     </div>
   );
 };
